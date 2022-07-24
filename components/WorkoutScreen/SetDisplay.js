1d0e8567b2a72877d9bc6c84c747d935fc6d1db6
began91 <began91@yahoo.com>
2022-07-24 09:57:06 -0400

    Fixed next station/rest display bug

--- components/WorkoutScreen/SetDisplay.js
+++ components/WorkoutScreen/SetDisplay.js
@@ -1,16 +1,15 @@
 import styles from './SetDisplay.module.css';
 
-export default function SetDisplay(props) {
-    const workout = props.workout;
-    const currentSet = props.currentSet;
-
+export default function SetDisplay({ workout, currentSet }) {
     const isWork = workout.stationIndex[currentSet] < workout.stations;
     const currentStation =
         workout.stationList[workout.stationIndex[currentSet]];
     const nextStation =
         workout.stationList[workout.stationIndex[currentSet + 1]];
     const stayOrMove =
-        currentSet === workout.stations ? 'Stay Here: ' : 'Next Station: ';
+        workout.stationIndex[currentSet] === workout.stations
+            ? 'Stay Here: '
+            : 'Next Station: ';
 
     return isWork ? (
         <div className={styles.workoutName}>{currentStation}</div>
