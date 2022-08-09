0d5e057bf33d63d1800e9c24ab6554b05f89fef8
began91 <began91@yahoo.com>
2022-08-09 00:19:12 -0400

    fixed index linting

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,47 +1,53 @@
-import Head from 'next/head';
+// import Head from 'next/head';
 import Link from 'next/link';
-import Layout, { siteTitle } from '../components/Layout';
-import utilStyles from '../styles/utils.module.css';
-import styles from '../styles/Home.module.css';
+import { Dispatch, SetStateAction } from 'react';
+import Layout from 'components/Layout';
+import utilStyles from 'styles/utils.module.css';
+// import styles from 'styles/Home.module.css';
 
-export default function Home({useDate: [date,setDate]}) {
-    return (
-        <Layout page="Home" date={date}>
-            <h1 className={utilStyles.headingXL}>
-                Fit 45-Minute Workout Timer
-            </h1>
-            <section className={utilStyles.headingMd}>
-                <h4>Start your fitness journey here:</h4>
-                <ul>
-                    <li>
-                        Get a sneak peak at the upcoming F45{' '}
-                        <Link href="schedule">
-                            <a>class schedule</a>
-                        </Link>
-                    </li>
-                    <li>
-                        See the workouts&apos;{' '}
-                        <Link href="weekly">
-                            <a>stations for this week</a>
-                        </Link>
-                    </li>
-                    <li>
-                        Build your own{' '}
-                        <Link href="custom">
-                            <a>custom workouts</a>
-                        </Link>{' '}
-                        using the F45 format.
-                    </li>
-                </ul>
-            </section>
-            <section className={utilStyles.headingXSm}>
-                This site is not affiliated with{' '}
-                <a href="https://f45training.com">F45 Training.</a> All
-                presented information is sourced from{' '}
-                <a href="https://www.reddit.com/r/f45">reddit.com/r/f45.</a> Any
-                reference to content related to the registered trade mark of F45
-                is for informational purposes only.
-            </section>
-        </Layout>
-    );
+interface HomeType {
+	useDate: [Date, Dispatch<SetStateAction<Date | undefined>>];
+}
+
+export default function Home({ useDate }: HomeType) {
+	const date = useDate[0];
+	return (
+		<Layout page="Home" date={date}>
+			<h1 className={utilStyles.headingXL}>
+				Fit 45-Minute Workout Timer
+			</h1>
+			<section className={utilStyles.headingMd}>
+				<h4>Start your fitness journey here:</h4>
+				<ul>
+					<li>
+						Get a sneak peak at the upcoming F45{' '}
+						<Link href="schedule">
+							<a>class schedule</a>
+						</Link>
+					</li>
+					<li>
+						See the workouts&apos;{' '}
+						<Link href="weekly">
+							<a>stations for this week</a>
+						</Link>
+					</li>
+					<li>
+						Build your own{' '}
+						<Link href="custom">
+							<a>custom workouts</a>
+						</Link>{' '}
+						using the F45 format.
+					</li>
+				</ul>
+			</section>
+			<section className={utilStyles.headingXSm}>
+				This site is not affiliated with{' '}
+				<a href="https://f45training.com">F45 Training.</a> All
+				presented information is sourced from{' '}
+				<a href="https://www.reddit.com/r/f45">reddit.com/r/f45.</a> Any
+				reference to content related to the registered trade mark of F45
+				is for informational purposes only.
+			</section>
+		</Layout>
+	);
 }
