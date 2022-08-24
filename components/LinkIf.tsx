440e55402e1ac3df88a9628aa5308a02bb90ff88
began91 <began91@yahoo.com>
2022-08-24 00:09:34 -0400

    Fixed type error for href

--- components/LinkIf.tsx
+++ components/LinkIf.tsx
@@ -1,11 +1,17 @@
 import Link from 'next/link';
+import React from 'react';
 
-export default function LinkIf({href,children,isLink=true}) {
-    
-    if (isLink) {//only make it a link if isLink is true
-        return <Link href={href}>{children}</Link>
-    }
+interface LinkIfType {
+	href: string;
+	children: any;
+	isLink: boolean;
+}
 
-    return <>{children}</>
-    
+export default function LinkIf({ href, children, isLink = true }: LinkIfType) {
+	if (isLink) {
+		//only make it a link if isLink is true
+		return <Link href={href}>{children}</Link>;
+	}
+
+	return <>{children}</>;
 }
